1aeae7b38448ef1e8f5bafc7742caa1c797286d7
ibrar-balosh <ibrarkurd3@gmail.com>
2026-04-05 11:41:46 +0500

    done

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StartScreen = ({ onStart }) => {
   return (
     <div className="glass-panel">
-      <h1 className="title">React Mastery Quiz</h1>
+      <h1 className="title">Quiz App</h1>
       <p className="subtitle">Test your knowledge of modern React and Vite.</p>
       <button className="btn-primary" onClick={onStart}>
         Start Quiz
